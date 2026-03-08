56e973ce154acd7f58c4a3251f8ec6bf63e2d691
Nathan Butler <git@nathanbutler.dev>
2026-03-09 07:36:52 +1100

    feat: scaffold opxBlazor Blazor Server project with MudBlazor

---
+++ apps/opxBlazor/Program.cs
@@ -0,0 +1,24 @@
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<opxBlazor.App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
